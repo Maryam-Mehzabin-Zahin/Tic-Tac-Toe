06104f57db5bdf29abcb47135df78a7f6584417c
Maryam Mehzabin Zahin <maryam.zahin@betpawa.com>
2023-12-24 18:04:47 +0200

    print board with actual entries

--- Tic-Tac-Toe/src/TicTacToe.java
+++ Tic-Tac-Toe/src/TicTacToe.java
@@ -12,21 +12,34 @@ public class TicTacToe {
     }
 
     private static void initiateBoard() {
+        System.out.println("\nTic Tac Toe Board");
         int cell = 0;
         for(int i = 0; i < 3; i++ ) {
             for(int j = 0; j < 3; j++) {
                 board.put(cell, ' ');
                 System.out.print("?");
-                if(j<2) System.out.print(" |");
+                if (j < 2) System.out.print(" |");
                 cell++;
             }
-            if(cell > 8 ) break;
+            if(cell > 8) break;
             System.out.println();
             System.out.println("--+--+--");
         }
     }
     private static void printBoard() {
-        
+        System.out.println("\nTic Tac Toe Board");
+        for (int i = 0; i < board.size(); i++) {
+            if(i % 3 == 0 && i > 0 ) {
+                System.out.println("\n--+--+--");
+            }
+            if(board.get(i) == ' ') {
+                System.out.print("?");
+            } else {
+                System.out.println(board.get(i));
+            }
+            if(i % 3 != 2) {
+                System.out.print(" |");
+            }
+        }
     }
-
 }
