b9371ce06c2ade224f5dcdb6a639013261c419d4
Maryam Mehzabin Zahin <maryam.zahin@betpawa.com>
2024-01-20 21:58:27 +0200

    * rowchecker

--- Tic-Tac-Toe/src/GameLogic.java
+++ Tic-Tac-Toe/src/GameLogic.java
@@ -2,7 +2,54 @@ import java.util.HashMap;
 
 public class GameLogic {
 
-    public void checkResult(final HashMap<Integer, Character> board, final int i, final char x) {
+    private  HashMap<Integer, Character> board;
 
+    public void checkResult(HashMap<Integer, Character> board, final int position, final char ch) {
+        this.board = board;
+        checkRowWise(position, ch);
+
+    }
+
+    private void checkRowWise(int position, char ch) {
+        if(position < 3) {
+            checkFirstRow(ch);
+        } else if (position > 3 && position < 6) {
+            checkSecondRow(ch);
+        }else {
+            checkThirdRow(ch);
+        }
+    }
+
+    private void checkFirstRow(char ch) {
+        for(int i = 0; i < 3; i++) {
+            if(board.get(i) != ch) return;
+        }
+        selectWinner(ch);
+        System.exit(0);
+    }
+
+    private void checkSecondRow(char ch) {
+        for(int i = 3; i < 6; i++) {
+            if(board.get(i) != ch) return;
+        }
+        selectWinner(ch);
+        System.exit(0);
+    }
+
+    private void checkThirdRow(char ch) {
+        for(int i = 6; i < 9; i++) {
+            if(board.get(i) != ch) return;
+        }
+        selectWinner(ch);
+        System.exit(0);
+    }
+
+    private static void selectWinner(final char ch) {
+        if(ch == 'X'){
+            System.out.println("\nGame over. You won.");
+        }
+        if(ch == 'O') {
+            System.out.println("\nGame over. Computer won.");
+        }
     }
 }
